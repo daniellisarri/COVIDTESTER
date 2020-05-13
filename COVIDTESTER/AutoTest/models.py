1f093d59921aa3da81579bffbf9569cb75336edc
daniellisarri <daniel.lisarri.ihs@gmail.com>
2020-05-13 16:54:34 +0200

    Creación de modelos Usuario y Formulario

--- COVIDTESTER/AutoTest/models.py
+++ COVIDTESTER/AutoTest/models.py
@@ -2,13 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-### Crear clase Formulario
-### Crear clase Usuario
+### Modelo Usuario
+# Contiene edad, sexo, código postal y (opcionalmente) nº de teléfono
+class Usuario(models.Model):
+    edad = models.IntegerField()
+    sexo = models.CharField(max_length=1)
+    cp = models.CharField(max_length=5)
+    telefono = models.CharField(max_length=7, null=True, blank=True)
 
-class Formulario(object):
-    def __init__(self, nombre, apellido):
-        self.nombre = nombre
+### Modelo Formulario
+# Contiene los síntomas en campos booleanos
+class Formulario(models.Model):
+    fiebre = models.BooleanField()
+    tos_seca = models.BooleanField()
+    axfixia = models.BooleanField()
+    perdida_sentidos = models.BooleanField()
+    repentino = models.BooleanField()
 
-class Usuario(object):
-    def __init__(self, nombre, apellido):
-        self.nombre = nombre
--- COVIDTESTER/COVIDTESTER/settings.py
+++ COVIDTESTER/COVIDTESTER/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', # IMPORTANTE para variables estáticas
+    'AutoTest',
 ]
 
 MIDDLEWARE = [
